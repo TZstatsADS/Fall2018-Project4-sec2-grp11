ede6f6116e141bb00bfa954f59753d989ac869df
JudyCom <40449861+JudyCom@users.noreply.github.com>
2018-11-27 22:29:35 -0500

    Add files via upload

--- lib/count_char.R
+++ lib/count_char.R
@@ -4,9 +4,9 @@ count_char <- function(vec){
   chars_useful <- chars[grepl("[a-zA-Z]|[[:digit:]]|[[:space:]]", chars)]
   chars_useful <- chars_useful[chars_useful != "\f"]
   chars_useful <- ifelse(chars_useful==" ", "space", chars_useful)
-  chars_useful <- factor(chars_useful, levels = c(letters, LETTERS, "space"))
+  chars_useful <- factor(chars_useful, levels = c(letters, LETTERS, "space", "0","1","2","3","4","5","6","7","8","9"))
   count_table <- table(chars_useful)
   cnts <- as.vector(count_table)
-  names(cnts) <- c(letters, LETTERS, "space")
+  names(cnts) <- c(letters, LETTERS, "space", "0","1","2","3","4","5","6","7","8","9")
   return(cnts)
 }
