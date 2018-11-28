89e958e6d8dff03bacc04ec541320119a744ee37
JudyCom <40449861+JudyCom@users.noreply.github.com>
2018-11-28 00:53:10 -0500

    Add files via upload

--- lib/count_char.R
+++ lib/count_char.R
@@ -10,3 +10,18 @@ count_char <- function(vec){
   names(cnts) <- c(letters, LETTERS, "space", "0","1","2","3","4","5","6","7","8","9")
   return(cnts)
 }
+
+# test_ground_truth_char_table <- count_char(unlist(test_ground_truth_list_vec))
+# test_tesseract_char_table <- count_char(unlist(test_tesseract_list_vec))
+# old_char_ratio <- test_tesseract_char_table/test_ground_truth_char_table
+# old_char_ratio_sorted <- sort(old_char_ratio, decreasing = TRUE)
+# old_char_ratio_sorted
+# old_char_ratio_sorted_mat <- matrix(old_char_ratio_sorted, ncol = 9, nrow = 7, byrow = TRUE)
+# barplot(old_char_ratio_sorted)
+# old_char_ratio_sorted_df <- as.data.frame(old_char_ratio_sorted)
+# names(old_char_ratio_sorted_df) <-"ratio"
+# old_char_ratio_sorted_df$character <- factor(rownames(old_char_ratio_sorted_df), levels = names(old_char_ratio_sorted))
+# library(ggplot2)
+# ggplot(data = old_char_ratio_sorted_df)+
+#   geom_bar(stat = "identity", aes(x=character,y = ratio))+
+#   ggtitle("Ratio of tesseract to ground truth for number of each alphanumeric character")
